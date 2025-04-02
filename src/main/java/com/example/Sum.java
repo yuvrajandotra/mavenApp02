baf3976ca2757a93562dde6606d24ba06185ef74
t-a-nmay <tanmaypndit@gmail.com>
2025-04-03 00:16:05 +0530

    initial commit

---
+++ src/main/java/com/example/Sum.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sum 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "sum of two numbers is : "+(5+6) );
+    }
+}
